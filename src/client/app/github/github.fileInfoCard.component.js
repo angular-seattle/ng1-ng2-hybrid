362af50373516b69af129805c5bc7fbe1e3e27d2
Jens Bodal <jensbodal@gmail.com>
2017-06-13 00:40:57 -0700

    cleanup file comparison

--- src/client/app/github/github.fileInfoCard.component.js
+++ src/client/app/github/github.fileInfoCard.component.js
@@ -22,9 +22,21 @@
     }
 
     function $onChanges(changesObj) {
-     if (changesObj.fileInfo) {
-        vm.fileInfo = angular.copy(changesObj.fileInfo.currentValue);
+      if (changesObj.fileInfo) {
+        var _fileInfo = angular.copy(changesObj.fileInfo.currentValue);
+
+        if (_fileInfo.patch) {
+          vm.patchInfo = formatPatch(_fileInfo.patch);
+          delete _fileInfo.patch;
+        }
+
+        vm.fileInfo = _fileInfo;
       }
+    }
+
+    function formatPatch(patch) {
+      patch = patch.replace(/\n\+/g, '\n');
+      return patch;
     }
   }
 })();
