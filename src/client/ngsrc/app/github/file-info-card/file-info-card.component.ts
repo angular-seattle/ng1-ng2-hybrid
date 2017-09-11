faa2b2d1c2336715a669f5f342c231392976c02c
Jens Bodal <jensbodal@gmail.com>
2017-09-11 11:05:12 -0700

    rename to file-info-card

--- src/client/ngsrc/app/github/file-info-card/file-info-card.component.ts
+++ src/client/ngsrc/app/github/file-info-card/file-info-card.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
-import { GithubFileInfo } from './githubFileInfo';
+import { GithubFileInfo } from './github-file-info';
 
 @Component({
   selector: 'aseed-file-info-card',
-  templateUrl: './fileInfoCard.component.html',
-  styleUrls: ['./fileInfoCard.component.scss']
+  templateUrl: './file-info-card.component.html',
+  styleUrls: ['./file-info-card.component.scss']
 })
 
 export class FileInfoCardComponent implements OnChanges {
