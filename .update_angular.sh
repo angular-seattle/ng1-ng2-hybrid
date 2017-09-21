69f87de1e524091a937d1dc0da99453ce35d5c7e
Jens Bodal <jensbodal@gmail.com>
2017-09-21 09:20:02 -0700

    update angular to 4.2.4 and cli to 1.2.7

---
+++ .update_angular.sh
@@ -0,0 +1,16 @@
+angular_version="4.2.4"
+
+yarn add -E @angular/core@$angular_version \
+  @angular/common@$angular_version \
+  @angular/compiler@$angular_version \
+  @angular/forms@$angular_version \
+  @angular/http@$angular_version \
+  @angular/platform-browser@$angular_version \
+  @angular/platform-browser-dynamic@$angular_version \
+  @angular/platform-server@$angular_version \
+  @angular/router@$angular_version \
+  @angular/upgrade@$angular_version \
+  @angular/animations@$angular_version \
+
+yarn add -ED @angular/compiler-cli@$angular_version \
+  @angular/language-service@$angular_version
